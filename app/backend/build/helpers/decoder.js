4e08c978db13c590038439a4282a6cb927b27323
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2022-10-29 16:14:31 -0300

    Fix: Build backend container

--- app/backend/build/helpers/decoder.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsonwebtoken_1 = require("jsonwebtoken");
-function decodeToken(token) {
-    const { payload } = (0, jsonwebtoken_1.verify)(token, process.env.JWT_SECRET);
-    return payload;
-}
-exports.default = decodeToken;
-//# sourceMappingURL=decoder.js.map
